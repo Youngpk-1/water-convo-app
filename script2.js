765b097d7d3ff415ff037d92f1372d17d25b0c0d
isaacwatson30 <isaacwatson30@gmail.com>
2025-09-04 00:31:33 -0500

    added buttons with translation on page 2.html and script2.js

--- script2.js
+++ script2.js
@@ -5,3 +5,33 @@ onEvent("waterbottle", "click", function () {
 onEvent("emptybottle", "click", function () {
   setProperty("emptybottle", "background-color", "lightblue");
 });
+setProperty("waterAppheader", "color", "blue");
+onEvent("spanish", "click", toSpanish);
+
+onEvent("english", "click", toEnglish);
+
+function toEnglish() {
+  setText("waterAppheader", "Water Conservation Tips");
+  setText(
+    "text1",
+    "Instead of buying disposable water bottles, use a refillable bottle. Used water bottles fill our land fills and trap valuable water."
+  );
+  setText(
+    "text2",
+    "Choose the correct water bottle. Then click the faucet to fill."
+  );
+  setText("next", "Next");
+}
+
+function toSpanish() {
+  setText("waterAppheader", "Consejos para la conservación del agua");
+  setText(
+    "text1",
+    "En lugar de comprar botellas de agua desechables, usa una botella rellenable. Las botellas usadas llenan los vertederos y retienen agua valiosa."
+  );
+  setText(
+    "text2",
+    "Elige la botella de agua correcta. Luego, haz clic en el grifo para llenarla."
+  );
+  setText("next", "Próximo");
+}
